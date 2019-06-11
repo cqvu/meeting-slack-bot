0ede961ae2a58f095b3367bc567e9b8c88e09f1d
Glitch (hello-express) <none>
2019-06-11 04:15:00 +0000

    ⛄️🕵 Checkpoint ./test.py:1937196/534

---
+++ test.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import abort, Flask, jsonify, request
+
+app = Flask(__name__)
+
+def is_request_valid(request):
+    is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
+    is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+
+    return is_token_valid and is_team_id_valid
+
+@app.route('/test', methods=['POST'])
+def test():
+    if not is_request_valid(request):
+        abort(400)
+
+    return jsonify(
+            response_type='in_channel',
+            text='hello world!',
+    )
