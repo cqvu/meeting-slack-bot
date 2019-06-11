96f98e3e3ffb3b1bc0d754d2aa11c18a4398f66e
Glitch (hello-express) <none>
2019-06-11 04:58:01 +0000

    😬🙋 Checkpoint ./server.py:1937196/1257

--- server.py
+++ server.py
@@ -1,25 +1,42 @@
 import os
-
+from slackclient import SlackClient
 from flask import abort, Flask, jsonify, request
 
 app = Flask(__name__)
+slack_client = SlackClient(os.environ['SLACK_API_TOKEN'])
+
+slack_client.api_call("api.test")
+
+def list_channels():
+  channels_call = slack_client.api_call("channels.list")
+  if channels_call.get('ok'):
+    return channels_call['channels']
+  return None
 
 def is_request_valid(request):
-    is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
-    is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+  is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
+  is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
 
-    return is_token_valid and is_team_id_valid
-
+  return is_token_valid and is_team_id_valid
+'''
 @app.route('/test', methods=['POST'])
 def test():
-    if not is_request_valid(request):
-        abort(400)
+  if not is_request_valid(request):
+    abort(400)
 
-    payload = 
-    return jsonify(
-            response_type='in_channel',
-            text='hello world!',
-    )
+  payload = {
+    response_type = 'in_channel',
+    text = 'hello world!'
+  }
+    
+  return jsonify(payload)
+'''
 
 if __name__ == "__main__":
-	app.run()
+  channels = list_channels()
+  if channels:
+    print("Channels: ")
+    for c in channels:
+      print(c['name'] + " (" + c['id'] + ")")
+    else:
+      print("Unable to authenticate.")
