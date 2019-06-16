e650c867c2cd3bb308a8c1b8060c1582edcb65dd
Glitch (hello-express) <none>
2019-06-16 03:02:05 +0000

    🗻🍵 Checkpoint ./server.py:1937196/3459 ./cool-file.py:1937196/1550

--- server.py
+++ server.py
@@ -6,58 +6,10 @@ app = Flask(__name__)
 slack_api_client = SlackClient(os.environ['SLACK_API_TOKEN'])
 slack_bot_client = SlackClient(os.environ['SLACK_BOT_TOKEN'])
 
-slack_api_client.api_call("api.test")
-
-'''
-def list_channels():
-  channels_call = slack_api_client.api_call("channels.list")
-  if channels_call.get('ok'):
-    return channels_call['channels']
-  return None
-'''
-
+@app.route('/', methods=['GET'])
 def is_request_valid(request):
   is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
   is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+  if is_token_valid and is_team_id_valid:
+    return "Success"
   return is_token_valid and is_team_id_valid
-
-@app.route('/', methods=['POST'])
-def test():
-  print("in test")
-  if not is_request_valid(request):
-    print("not valid")
-    abort(400)
-
-  payload = {
-    response_type = 'in_channel',
-    text = 'hello world!'
-  }
-    
-  return jsonify(
-    response_type='in_channel',
-    text='<https://youtu.be/frszEJb0aOo|General Kenobi!>',)
-
-'''
-def send_message(channel_id, message):
-  slack_api_client.api_call(
-    "chat.postMessage",
-    channel=channel_id,
-    text=message,
-    username='pythonbot',
-    icon_emoji=':robot_face:'
-  )
-'''
-
-if __name__ == "__main__":
-  '''
-  channels = list_channels()
-  if channels:
-    print("Channels: ")
-    for c in channels:
-      print(c['name'] + " (" + c['id'] + ")")
-      if c['name'] == 'general':
-        send_message(c['id'], "Hello " + c['name'] + "!")
-  else:
-    print("Unable to authenticate.")
-  '''
-  app.run()
