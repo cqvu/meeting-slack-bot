5183557ec26d2bfc906727e33ed7686315e89066
Glitch (hello-express) <none>
2019-06-16 03:24:04 +0000

    🐞🍣 Checkpoint ./server.py:1937196/343

--- server.py
+++ server.py
@@ -3,13 +3,16 @@ from slackclient import SlackClient
 from flask import abort, Flask, jsonify, request
 
 app = Flask(__name__)
-slack_api_client = SlackClient(os.environ['SLACK_API_TOKEN'])
-slack_bot_client = SlackClient(os.environ['SLACK_BOT_TOKEN'])
 
 @app.route('/', methods=['GET'])
+def verify():
+  print("Running")
+  return "Success"
+
 def is_request_valid(request):
   is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
   is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
-  if is_token_valid and is_team_id_valid:
-    return "Success"
   return is_token_valid and is_team_id_valid
+
+if __name__ == "__main__":
+  app.run()
