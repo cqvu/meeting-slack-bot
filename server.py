62a3af9658edf4bf327183b699daf053f43551fe
Glitch (hello-express) <none>
2019-06-30 07:59:06 +0000

    🐡🐬 Checkpoint ./server.py:1937196/4

--- server.py
+++ server.py
@@ -75,7 +75,7 @@ def createnote():
   g_login.LocalWebserverAuth()
   drive = GoogleDrive(g_login)
   
-  file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+  file1 = drive.CreateFile({'title': 'Hello'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
   file1.SetContentString('Hello World!') # Set content of the file from given string.
   file1.Upload()
   
