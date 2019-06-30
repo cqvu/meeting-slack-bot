a663140f22b069771d180d61b09ced0c065c67ac
Glitch (hello-express) <none>
2019-06-30 06:45:07 +0000

    🌄👖 Checkpoint ./server.py:1937196/1352

--- server.py
+++ server.py
@@ -40,40 +40,9 @@ def createnote():
     print("not valid")
     abort(400)
   
-  '''
-  creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-  if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-      creds = pickle.load(token)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-      creds = flow.run_local_server()
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-      pickle.dump(creds, token)
-      
-  service = build('docs', 'v1', credentials=creds)
-  print("in createnote")
-  title = 'Test Doc'
-  body = {
-    'title': title
-  }
-  doc = service.documents() \
-    .create(body=body).execute()
-  print('Created document with title: {0}'.format(
-    doc.get('title')))
-  '''
-  
-  g_login = GoogleAuth()
-  g_login.LocalWebserverAuth()
-  drive = GoogleDrive(g_login)
+  file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+  file1.SetContentString('Hello World!') # Set content of the file from given string.
+  file1.Upload()
   
 @app.route('/remindnotes', methods=['POST'])
 def remindnotes():
